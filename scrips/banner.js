e5a0b84a5d4cb4b746376ce112f0483be07c5a18
Lara Bovens <114385168+LaraBovens5@users.noreply.github.com>
2023-10-31 20:08:27 +0100

    Add files via upload

---
+++ scrips/banner.js
@@ -0,0 +1,30 @@
+// Navigation links 
+const navLinks = document.querySelectorAll(".navbarManus .nav-link");
+
+function handleClick(e) {
+  // Remove 'active' class from all navigation links
+  navLinks.forEach(link => link.classList.remove("active"));
+
+  // Add 'active' class to the clicked navigation link
+  e.target.classList.add("active");
+}
+
+navLinks.forEach(link => {
+  link.addEventListener("click", handleClick);
+});
+// Banner automatic images 
+const bannerImage = document.getElementById("banner-image");
+const images = ["/Users/bovenslara/Desktop/Manus/manus-nl/images/index_banner1_quote_1.png", "/Users/bovenslara/Desktop/Manus/manus-nl/images/index_banner1_quote_2.png", "/Users/bovenslara/Desktop/Manus/manus-nl/images/index_banner1_quote_3.png","/Users/bovenslara/Desktop/Manus/manus-nl/images/index_banner1_quote_4.png", "/Users/bovenslara/Desktop/Manus/manus-nl/images/index_banner1_quote_5.png"];
+let currentImageIndex = 0;
+
+function changeImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  bannerImage.style.opacity = 0;
+  
+  setTimeout(() => {
+    bannerImage.src = images[currentImageIndex];
+    bannerImage.style.opacity = 1;
+  }, 500);
+}
+
+setInterval(changeImage, 4000); // Change image every 45 seconds
